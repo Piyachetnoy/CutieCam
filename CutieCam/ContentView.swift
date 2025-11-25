0f6fc70eee119492edd4707d455225a70df14b3d
Piyachetnoy <piyachet2310noy@gmail.com>
2025-11-25 17:26:51 +0900

    Initial Commit

---
+++ CutieCam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CutieCam
+//
+//  Created by Piyachet Pongsantichai on 2025/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CutieCam/CutieCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  CutieCamApp.swift
+//  CutieCam
+//
+//  Created by Piyachet Pongsantichai on 2025/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CutieCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
